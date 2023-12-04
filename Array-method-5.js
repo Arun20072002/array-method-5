8334747d13834af11dd9f4d45b27d345fd3895be
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:16:36 +0530

    Array-method-5

---
+++ Array-method-5.js
@@ -0,0 +1,12 @@
+document.write("5. Get the index values of  ['dairymilk', 'ferroro', 'galaxy']"+"<br>")
+document.write("----------------------------------------------------------------------"+"<br>")
+
+ var chocolates = ['dairymilk', 'ferroro', 'galaxy', 'twix', 'mars', 'bounty'];
+//  document.write(chocolates+"<br>")
+
+var a=chocolates.indexOf("dairymilk")
+document.write(a+"&nbsp")
+var b=chocolates.indexOf("ferroro")
+document.write(b+"&nbsp")
+var c=chocolates.indexOf("galaxy")
+document.write(c+"&nbsp"+"<br>")
